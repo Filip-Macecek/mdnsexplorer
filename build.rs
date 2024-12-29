d599fa144988147d4fca32016743c022c59e50e2
Filip-Macecek <filip.macecek@mews.com>
2024-12-29 12:33:57 +0100

    Embed manifest

--- build.rs
+++ build.rs
@@ -1,4 +1,21 @@
+const APPLICATION_MANIFEST: &str = r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+    <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+        <security>
+            <requestedPrivileges>
+                <requestedExecutionLevel level="requireAdministrator" uiAccess="false"/>
+            </requestedPrivileges>
+        </security>
+    </trustInfo>
+</assembly>"#;
+
 fn main() {
     // Specify the path to the directory containing `packet.lib`
     println!("cargo:rustc-link-search=native=libs");
+
+    #[cfg(windows)]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_manifest_file(APPLICATION_MANIFEST);
+    }
 }
